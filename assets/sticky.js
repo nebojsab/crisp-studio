75b2fc3f536d79a3358551882b2702168e3688dc
nebojsa <nebojsa@lemonbrew.com>
2020-05-30 16:42:54 +0200

    Finishing carousels

--- assets/sticky.js
+++ assets/sticky.js
@@ -8,10 +8,10 @@ var navbar = document.getElementById("header");
 var sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-    } else {
-        navbar.classList.remove("sticky");
-    }
-}
+// function myFunction() {
+//     if (window.pageYOffset >= sticky) {
+//         navbar.classList.add("sticky")
+//     } else {
+//         navbar.classList.remove("sticky");
+//     }
+// }
